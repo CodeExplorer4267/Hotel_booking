aafb113ddea88e4a62751a71772dda57c394a94f
CodeExplorer4267 <rupambhadra478@gmail.com>
2025-09-04 19:37:22 +0530

    commit3

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdDateRange } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
+import roomImg1 from "../../assets/roomImg1.png";
+import roomImg2 from "../../assets/roomImg2.png";
+import roomImg3 from "../../assets/roomImg3.png";
+import roomImg4 from "../../assets/roomImg4.png";
+import { assets } from "../../assets/assets";
+import { FaIndianRupeeSign } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -68,8 +74,124 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="featuredHotel p-10 flex flex-col gap-6">
-         <h1>Featured Hotel</h1>
+      <div
+        className="featuredHotel p-10 flex flex-col gap-6 w-[100%]"
+        style={{
+          backgroundColor: "rgb(223, 255, 254)",
+        }}
+      >
+        <h1 className="font-bold text-3xl text-black text-center">
+          Featured Hotels
+        </h1>
+        <p className="text-[15px] text-center">
+          Explore our handpicked selection of luxury hotels and resorts.
+        </p>
+        <div className="flex flex-row gap-8 justify-center items-center">
+          <div className="h-[300px] w-[270px] border-black border-2 rounded-[20px] flex flex-col">
+            <img
+              src={roomImg1}
+              alt=""
+              className="h-[55%] w-[100%] rounded-tl-[20px] rounded-tr-[20px]"
+            />
+            <div className="flex flex-row justify-between p-3">
+              <h2 className="text-[17px]">The Emberwood Inn</h2>
+              <div className="flex flex-row gap-1 items-center">
+                <img src={assets.starIconFilled} alt="" />
+                <span className="font-bold">4.6</span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start items-center pl-3 pr-3 gap-0.5">
+               <FaLocationDot className="inline-block mr-2 text-gray-400" />
+               <span className="text-gray-500 text-center">Delhi, India</span>
+            </div>
+            <div className="flex flex-row justify-between items-center p-3">
+              <div className="flex flex-row gap-1 items-center">
+                <FaIndianRupeeSign className="inline-block" />
+                <span className="font-bold">3500</span>
+                <span className="text-gray-500">/night</span>
+              </div>
+              <button className="bg-black text-white rounded-[10px] w-[105px] text-[14px] px-[7px] py-[3px]">View Details</button>
+            </div>
+          </div>
+          <div className="h-[300px] w-[270px] border-black border-2 rounded-[20px] flex flex-col">
+            <img
+              src={roomImg2}
+              alt=""
+              className="h-[55%] w-[100%] rounded-tl-[20px] rounded-tr-[20px]"
+            />
+            <div className="flex flex-row justify-between p-3">
+              <h2 className="text-[17px]">The Veridian Suite</h2>
+              <div className="flex flex-row gap-1 items-center">
+                <img src={assets.starIconFilled} alt="" />
+                <span className="font-bold">4.5</span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start items-center pl-3 pr-3 gap-0.5">
+               <FaLocationDot className="inline-block mr-2 text-gray-400" />
+               <span className="text-gray-500 text-center">Kolkata, India</span>
+            </div>
+            <div className="flex flex-row justify-between items-center p-3">
+              <div className="flex flex-row gap-1 items-center">
+                <FaIndianRupeeSign className="inline-block" />
+                <span className="font-bold">3000</span>
+                <span className="text-gray-500">/night</span>
+              </div>
+              <button className="bg-black text-white rounded-[10px] w-[105px] text-[14px] px-[7px] py-[3px]">View Details</button>
+            </div>
+          </div>
+          <div className="h-[300px] w-[270px] border-black border-2 rounded-[20px] flex flex-col">
+            <img
+              src={roomImg3}
+              alt=""
+              className="h-[55%] w-[100%] rounded-tl-[20px] rounded-tr-[20px]"
+            />
+            <div className="flex flex-row justify-between p-3">
+              <h2 className="text-[17px]">Aura Grand Hotel</h2>
+              <div className="flex flex-row gap-1 items-center">
+                <img src={assets.starIconFilled} alt="" />
+                <span className="font-bold">4.9</span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start items-center pl-3 pr-3 gap-0.5">
+               <FaLocationDot className="inline-block mr-2 text-gray-400" />
+               <span className="text-gray-500 text-center">Pune, India</span>
+            </div>
+            <div className="flex flex-row justify-between items-center p-3">
+              <div className="flex flex-row gap-1 items-center">
+                <FaIndianRupeeSign className="inline-block" />
+                <span className="font-bold">4000</span>
+                <span className="text-gray-500">/night</span>
+              </div>
+              <button className="bg-black text-white rounded-[10px] w-[105px] text-[14px] px-[7px] py-[3px]">View Details</button>
+            </div>
+          </div>
+          <div className="h-[300px] w-[270px] border-black border-2 rounded-[20px] flex flex-col">
+            <img
+              src={roomImg4}
+              alt=""
+              className="h-[55%] w-[100%] rounded-tl-[20px] rounded-tr-[20px]"
+            />
+            <div className="flex flex-row justify-between p-3">
+              <h2 className="text-[17px]">The Meridian Lodge</h2>
+              <div className="flex flex-row gap-1 items-center">
+                <img src={assets.starIconFilled} alt="" />
+                <span className="font-bold">4.7</span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start items-center pl-3 pr-3 gap-0.5">
+               <FaLocationDot className="inline-block mr-2 text-gray-400" />
+               <span className="text-gray-500 text-center">Hyderabad, India</span>
+            </div>
+            <div className="flex flex-row justify-between items-center p-3">
+              <div className="flex flex-row gap-1 items-center">
+                <FaIndianRupeeSign className="inline-block" />
+                <span className="font-bold">3700</span>
+                <span className="text-gray-500">/night</span>
+              </div>
+              <button className="bg-black text-white rounded-[10px] w-[105px] text-[14px] px-[7px] py-[3px]">View Details</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
